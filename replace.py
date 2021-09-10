b426d7be119f9361af91b6d44fa93e1a18b447bc
GilbertoNavaMarcos <gilberto.nava.marcos@outlook.com>
2021-09-10 16:12:49 -0500

    Meme 1

---
+++ replace.py
@@ -0,0 +1,12 @@
+#Ejercicio de reemplazar caracteres de una cadena
+#Meme: Hola Juan Carlos
+
+guion="Hola persona, como estas chupapija."
+print(guion)
+
+guion=guion.replace("persona","Juan Carlos")
+print(guion)
+
+#complementando el meme
+guion=guion.replace("."," te voy a matar cuando te vea")
+print(guion)
